7a4480ca2c691e893839ff7719a93fb23edfa727
herougo <herougo@gmail.com>
2024-10-22 16:47:06 -0400

    Flesh out LeftNav with icons

--- twitter-frontend/src/layouts/left-nav/LeftNav.js
+++ twitter-frontend/src/layouts/left-nav/LeftNav.js
@@ -1,29 +1,52 @@
 import React from 'react';
+import { BiBell, BiHomeAlt, BiLogOut, BiSearch, BiShoppingBag, BiUser } from 'react-icons/bi';
+import { FiMail } from 'react-icons/fi';
 import './LeftNav.css';
 
 const LeftNav = () => {
     return (
         <div className='left-nav holy-grail-left'>
-            <div>
-                Home
+            <div className='left-nav__btn'>
+                <div className='left-nav__icon-container'>
+                    <BiHomeAlt></BiHomeAlt>
+                </div>
+                <div className='left-nav__btn-text'>Home</div>
             </div>
-            <div>
-                Profile
+            <div className='left-nav__btn'>
+                <div className='left-nav__icon-container'>
+                    <BiUser></BiUser>
+                </div>
+                <div className='left-nav__btn-text'>Profile</div>
             </div>
-            <div>
-                Search
+            <div className='left-nav__btn'>
+                <div className='left-nav__icon-container'>
+                    <BiSearch></BiSearch>
+                </div>
+                <div className='left-nav__btn-text'>Search</div>
             </div>
-            <div>
-                Store
+            <div className='left-nav__btn'>
+                <div className='left-nav__icon-container'>
+                    <BiShoppingBag></BiShoppingBag>
+                </div>
+                <div className='left-nav__btn-text'>Store</div>
             </div>
-            <div>
-                Notifications
+            <div className='left-nav__btn'>
+                <div className='left-nav__icon-container'>
+                    <BiBell></BiBell>
+                </div>
+                <div className='left-nav__btn-text'>Notifications</div>
             </div>
-            <div>
-                Messages
+            <div className='left-nav__btn'>
+                <div className='left-nav__icon-container'>  
+                    <FiMail></FiMail>
+                </div>
+                <div className='left-nav__btn-text'>Messages</div>
             </div>
-            <div>
-                Logout
+            <div className='left-nav__btn'>
+                <div className='left-nav__icon-container'>
+                    <BiLogOut></BiLogOut>
+                </div>
+                <div className='left-nav__btn-text'>Logout</div>
             </div>
         </div>
     );
