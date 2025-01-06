0bcb72cadfb174e37696502ae6f6e6dba369b515
herougo <herougo@gmail.com>
2025-01-06 14:50:12 -0500

    feat: basic profile layout (WIP)

--- twitter-frontend/src/pages/main/content/profile/ProfilePageContent.js
+++ twitter-frontend/src/pages/main/content/profile/ProfilePageContent.js
@@ -4,7 +4,17 @@ import './ProfilePageContent.css';
 const ProfilePageContent = () => {
     return (
         <div>
-            Profile
+            <div className='profile-page-content__background'>
+                <img/>
+            </div>
+            <div className='profile-page-content__profile-pic-parent-div'>
+                <div className='profile-page-content__profile-pic'>
+                    <img/>
+                </div>
+            </div>
+            <div className='profile-page-content__name'>
+
+            </div>
         </div>
     );
 }
