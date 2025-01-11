fe1178247d96867ea7e6ccb6bc73af1062f3555b
herougo <herougo@gmail.com>
2025-01-11 16:19:20 -0500

    feat: added username cookie

--- twitter-frontend/src/hooks/useCookieUser.js
+++ twitter-frontend/src/hooks/useCookieUser.js
@@ -1,22 +1,26 @@
 import { useState } from "react";
 import { getCookie, setCookie } from '../lib/cookies';
 
-const COOKIE_NAME = 'jwt_token';
+const USERNAME_COOKIE_NAME = 'username';
+const TOKEN_COOKIE_NAME = 'jwt_token';
 
 const useCookieUser = () => {
-    const cookieVal = getCookie(COOKIE_NAME);
+    const usernameCookieVal = getCookie(USERNAME_COOKIE_NAME);
+    const tokenCookieVal = getCookie(TOKEN_COOKIE_NAME);
     let defaultUser = null;
-    if (cookieVal) {
-        defaultUser = {token: cookieVal};
+    if (tokenCookieVal) {
+        defaultUser = {username: usernameCookieVal, token: tokenCookieVal};
     }
 
     const [user, setUser] = useState(defaultUser);
     
     const newSetUser = (val) => {
         if (val === null) {
-            setCookie(COOKIE_NAME, '');
+            setCookie(USERNAME_COOKIE_NAME, '');
+            setCookie(TOKEN_COOKIE_NAME, '');
         } else {
-            setCookie(COOKIE_NAME, val.token);
+            setCookie(USERNAME_COOKIE_NAME, val.username);
+            setCookie(TOKEN_COOKIE_NAME, val.token);
         }
         setUser(val);
     }
