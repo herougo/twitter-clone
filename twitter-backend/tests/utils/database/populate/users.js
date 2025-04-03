b21c13530253724651d802200aa4125fcaba7704
herougo <herougo@gmail.com>
2025-04-03 19:42:33 -0400

    feat: Added JWT tokens to test data

--- twitter-backend/tests/utils/database/populate/users.js
+++ twitter-backend/tests/utils/database/populate/users.js
@@ -1,39 +1,18 @@
 const DI_NAMES = require("../../../../src/server/dependency-injection/names");
-const { DB_IDS } = require("../ids");
+const { USER_DATA } = require("../userData");
 
 const populateUsers = async (diContainer) => {
     const userRepository = diContainer.resolve(DI_NAMES.userRepository)
 
     const user = await userRepository.create(
-        {
-            _id: DB_IDS.mainUser,
-            username: "username",
-            passwordHash: "$2b$10$AbJt1PaeMfPOOuI7lmgiYOMYv2vL/WGtGV1TuMAjjSo0jAZt1hQ2.",
-            firstName: "hi",
-            lastName: "there",
-            email: "example@example.com"
-        }
+        USER_DATA.user
     );
     const follower = await userRepository.create(
-        {
-            _id: DB_IDS.followerUser,
-            username: "follower",
-            passwordHash: "$2b$10$AbJt1PaeMfPOOuI7lmgiYOMYv2vL/WGtGV1TuMAjjSo0jAZt1hQ2.",
-            firstName: "hello",
-            lastName: "you",
-            email: "follower@example.com"
-        }
+        USER_DATA.follower
     );
     await userRepository.addFollower(user, follower);
     const anotherUser = await userRepository.create(
-        {
-            _id: DB_IDS.anotherUser,
-            username: "another",
-            passwordHash: "$2b$10$AbJt1PaeMfPOOuI7lmgiYOMYv2vL/WGtGV1TuMAjjSo0jAZt1hQ2.",
-            firstName: "hiya",
-            lastName: "me",
-            email: "another@example.com"
-        }
+        USER_DATA.anotherUser
     );
 }
 
---
+++ twitter-backend/tests/utils/database/userData.js
@@ -0,0 +1,53 @@
+const jwt = require("jsonwebtoken");
+const CONFIG = require("../../../src/config");
+const { DB_IDS } = require("./ids");
+
+
+function logJWT(data) {
+    const tokenPayload = {
+        username: data.username,
+        id: data._id,
+        expectedPasswordHash: data.passwordHash
+    };
+    const secretKey = CONFIG.jwtSecretKey;
+    
+    console.log(jwt.sign(tokenPayload, secretKey));
+}
+
+
+const USER_DATA = {
+    user: {
+        _id: DB_IDS.mainUser,
+        username: "username",
+        passwordHash: "$2b$10$AbJt1PaeMfPOOuI7lmgiYOMYv2vL/WGtGV1TuMAjjSo0jAZt1hQ2.",
+        firstName: "hi",
+        lastName: "there",
+        email: "example@example.com"
+    },
+    follower: {
+        _id: DB_IDS.followerUser,
+        username: "follower",
+        passwordHash: "$2b$10$AbJt1PaeMfPOOuI7lmgiYOMYv2vL/WGtGV1TuMAjjSo0jAZt1hQ2.",
+        firstName: "hello",
+        lastName: "you",
+        email: "follower@example.com"
+    },
+    anotherUser: {
+        _id: DB_IDS.anotherUser,
+        username: "another",
+        passwordHash: "$2b$10$AbJt1PaeMfPOOuI7lmgiYOMYv2vL/WGtGV1TuMAjjSo0jAZt1hQ2.",
+        firstName: "hiya",
+        lastName: "me",
+        email: "another@example.com"
+    }
+};
+
+const USER_JWT_TOKENS = {
+    user: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXJuYW1lIiwiaWQiOiI2NzUzNTFkNjk4NDUyZTRmYWRjMzc2YmUiLCJleHBlY3RlZFBhc3N3b3JkSGFzaCI6IiQyYiQxMCRBYkp0MVBhZU1mUE9PdUk3bG1naVlPTVl2MnZML1dHdEdWMVR1TUFqalNvMGpBWnQxaFEyLiIsImlhdCI6MTc0MzcyMzM4OX0.S-m8EStyU7QWDwn9QRD5DUHPnONrZi-cDNLVby7DVUo',
+    follower: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImZvbGxvd2VyIiwiaWQiOiI2NzUzNTFkNjk4NDUyZTRmYWRjMzc2YzEiLCJleHBlY3RlZFBhc3N3b3JkSGFzaCI6IiQyYiQxMCRBYkp0MVBhZU1mUE9PdUk3bG1naVlPTVl2MnZML1dHdEdWMVR1TUFqalNvMGpBWnQxaFEyLiIsImlhdCI6MTc0MzcyMzM4OX0.3sI__5vTDqNU4cHfdGmnwZq6XdA7_jgfUFs7UVnppr0',
+    anotherUser: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFub3RoZXIiLCJpZCI6IjY3NTM1MWQ2OTg0NTJlNGZhZGMzNzZjMCIsImV4cGVjdGVkUGFzc3dvcmRIYXNoIjoiJDJiJDEwJEFiSnQxUGFlTWZQT091STdsbWdpWU9NWXYydkwvV0d0R1YxVHVNQWpqU28wakFadDFoUTIuIiwiaWF0IjoxNzQzNzIzMzg5fQ.0H3TNbsJl7EOLBss11MgIHtkhOgh1Jwe0Pc_lHiyxOw'
+};
+
+module.exports = {
+    USER_DATA, USER_JWT_TOKENS
+}
