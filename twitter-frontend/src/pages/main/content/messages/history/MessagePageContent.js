b8d929d4a7f12c8c0f0588d45e94cdaddba84193
herougo <herougo@gmail.com>
2025-04-09 17:56:04 -0400

    feat: added date groups to message history

--- twitter-frontend/src/pages/main/content/messages/history/MessagePageContent.js
+++ twitter-frontend/src/pages/main/content/messages/history/MessagePageContent.js
@@ -3,29 +3,55 @@ import './MessagePageContent.css';
 import ChannelMessage from './ChannelMessage';
 import CreateMessage from './CreateMessage';
 import MessageRecipientInfo from './MessageRecipientInfo';
+import humanReadableDate from '../../../../../utils/humanReadableDate';
 
 const messagesData = [
     {
         id: 0,
         content: 'Hello there.',
+        createdAt: '2025-04-05T21:12:32.069+00:00',
         isFromYou: true
     },
     {
         id: 1,
         content: 'Hi there.',
+        createdAt: '2025-04-05T21:12:32.069+00:00',
         isFromYou: false
     },
     {
         id: 2,
         content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        createdAt: '2025-04-06T21:12:32.069+00:00',
         isFromYou: true
     },
     {
         id: 3,
         content: "Oh hai!",
+        createdAt: '2025-04-07T21:12:32.069+00:00',
         isFromYou: true
     }
-]
+];
+
+const groupMessagesByDate = (messages) => {
+    const messageDates = [];
+    const messageGroups = [];
+
+    let prevDate = null;
+    let currentDate = null;
+    for (let message of messages) {
+        currentDate = humanReadableDate(message.createdAt);
+        if (currentDate !== prevDate) {
+            prevDate = currentDate;
+            messageDates.push(currentDate);
+            messageGroups.push([]);
+        }
+        messageGroups[messageGroups.length - 1].push(message);
+    }
+
+    return {
+        messageDates, messageGroups
+    };
+};
 
 const MessagePageContent = () => {
     const [messages, setMessages] = useState(messagesData);
@@ -34,9 +60,12 @@ const MessagePageContent = () => {
         setMessages([...messages, {
             id: messages.length,
             isFromYou: true,
+            createdAt: '2025-04-07T21:12:32.069+00:00',
             content: message.content
         }]);
     }
+
+    const {messageDates, messageGroups} = groupMessagesByDate(messages);
 
     return (
         <div className='message-page-content'>
@@ -44,13 +73,24 @@ const MessagePageContent = () => {
                 <MessageRecipientInfo />
                 <div className='message-page-content__messages'>
                     {
-                        messages.map(
-                            message => (
-                                <ChannelMessage
-                                    key={message.id}
-                                    isFromYou={message.isFromYou}>
-                                    {message.content}
-                                </ChannelMessage>
+                        messageGroups.map(
+                            (messageGroup, ix) => (
+                                <div>
+                                    <div className='message-page-content__message-group-date'>
+                                        {messageDates[ix]}
+                                    </div>
+                                    <div className='message-page-content__message-group'>
+                                        {messageGroup.map(
+                                            message => (
+                                                <ChannelMessage
+                                                    key={message.id}
+                                                    isFromYou={message.isFromYou}>
+                                                    {message.content}
+                                                </ChannelMessage>
+                                            )
+                                        )}
+                                    </div>
+                                </div>
                             )
                         )
                     }
