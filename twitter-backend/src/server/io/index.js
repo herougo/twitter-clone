7c58b308ea4971b6cbabce2e26232a09c424c641
herougo <herougo@gmail.com>
2025-04-16 21:17:52 -0400

    misc: added console.log to backend socket.io functions

--- twitter-backend/src/server/io/index.js
+++ twitter-backend/src/server/io/index.js
@@ -1,11 +1,14 @@
 const applySocketIo = (io) => {
     io.on('connection', (socket) => {
+        console.log(`Socket ${socket.id} connected`);
         socket.on('join-room', (userId) => {
             socket.join(userId);
+            console.log(`Socket ${socket.id} joined room ${userId}`);
             socket.emit('connected');
         });
         socket.on('leave-room', (userId) => {
             socket.leave(userId);
+            console.log(`Socket ${socket.id} left room ${userId}`);
         })
 
     });
